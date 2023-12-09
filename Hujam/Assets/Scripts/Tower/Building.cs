82efa0e131ceed6255de4d99abccd4a7010049e3
DisketQ <disketofficial@gmail.com>
2023-12-09 21:48:29 +0300

    Visuals
    Grass
    Towers

---
+++ Hujam/Assets/Scripts/Tower/Building.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Towers
+{
+    public class Building : MonoBehaviour
+    {
+        public BuildingData BuildingData;
+    }
+   
+}
