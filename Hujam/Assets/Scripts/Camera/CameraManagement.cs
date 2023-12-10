db50358f8e393d1df4a409da890d0d7ba17ec980
DisketQ <disketofficial@gmail.com>
2023-12-11 01:52:14 +0300

    CameraManagement

--- Hujam/Assets/Scripts/Camera/CameraManagement.cs
+++ Hujam/Assets/Scripts/Camera/CameraManagement.cs
@@ -8,32 +8,47 @@ public class CameraManagement : MonoBehaviour
     [SerializeField] private float _borderSize = 100f;
     [SerializeField] private float _moveSpeed = 40f;
     private Vector3 _mousePosition;
-    Vector3 _moveDirection;
 
-    [SerializeField] private Vector3 minMax;
+    [SerializeField] private Vector2 minMaxX;
+    [SerializeField] private Vector2 minMaxZ;
 
     private void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
 
+        Vector3 moveVector;
 
-        if (mousePosition.x < _borderSize || mousePosition.x > Screen.width - _borderSize || mousePosition.y < _borderSize || mousePosition.y > Screen.height - _borderSize)
+        if (mousePosition.x < _borderSize ||  mousePosition.x > Screen.width - _borderSize || mousePosition.y < _borderSize || mousePosition.y > Screen.height - _borderSize)
         {
-            _moveDirection = GetMoveDirection(mousePosition);
+            moveVector = GetMoveDirectionMouse(mousePosition);
         }
         else
         {
-            _moveDirection = GetMoveDirection();
+            moveVector = GetMoveDirectionKeyboard();
         }
-
-        transform.Translate(_moveDirection * _moveSpeed * Time.deltaTime);
-
-        transform.position = Extension.Clamp(transform.position , minMax);
+        
+        Move(moveVector);
 
     }
 
+    private void Move(Vector3 moveVector)
+    {
+        transform.position += moveVector * (_moveSpeed * Time.deltaTime);
 
-    Vector3 GetMoveDirection(Vector3 moveVector)
+        transform.position = Extension.Clamp(transform.position , minMaxX,minMaxZ);
+    }
+
+    Vector3 GetMoveDirectionKeyboard()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.z = Input.GetAxisRaw("Vertical");
+
+        return moveDirection;
+    }
+
+    Vector3 GetMoveDirectionMouse(Vector3 mousePosition)
     {
         Vector3 moveDirection = Vector3.zero;
 
@@ -51,26 +66,6 @@ public class CameraManagement : MonoBehaviour
             moveDirection.z = -1;
         }
         else if (mousePosition.z > Screen.height - _borderSize)
-        {
-            moveDirection.z = 1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveDirection.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveDirection.x = 1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            moveDirection.z = -1;
-        }
-
-        if (Input.GetKey(KeyCode.W))
         {
             moveDirection.z = 1;
         }
--- Hujam/Assets/Scripts/Extension/Extension.cs
+++ Hujam/Assets/Scripts/Extension/Extension.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public static class Extension
 {
-    public static Vector3 Clamp(Vector3 vector, Vector3 clampVector)
+    public static Vector3 Clamp(Vector3 vector, Vector2 clampVectorX,Vector2 clampVectorZ)
     {
-        float x = Mathf.Clamp(vector.x, -clampVector.x, clampVector.x);
-        float y = Mathf.Clamp(vector.y, -clampVector.y, clampVector.y);
-        float z = Mathf.Clamp(vector.z, -clampVector.z, clampVector.z);
+        float x = Mathf.Clamp(vector.x, clampVectorX.x, clampVectorX.y);
+        float y = Mathf.Clamp(vector.y, float.NegativeInfinity, float.PositiveInfinity);
+        float z = Mathf.Clamp(vector.z, clampVectorZ.x, clampVectorZ.y);
 
         return new Vector3(x, y, z);    
     }
