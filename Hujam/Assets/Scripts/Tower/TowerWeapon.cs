fa71bcf42729a2e734ff34a9e327b27567d17eab
DisketQ <disketofficial@gmail.com>
2023-12-11 12:26:43 +0300

    Towers

--- Hujam/Assets/Scripts/Tower/TowerWeapon.cs
+++ Hujam/Assets/Scripts/Tower/TowerWeapon.cs
@@ -1,6 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class TowerWeapon : MonoBehaviour
 {
         public LayerMask layerMask;
+        private Transform _transform;
+        public float radius;
+        public float damage;
+
+        private void Awake()
+        {
+                _transform = transform;
+        }
+
+        private void Update()
+        {
+                Collider[] colliders = Physics.OverlapSphere(_transform.position, radius, layerMask);
+
+                if (colliders.Length > 0)
+                {
+                        IDamagable damagable = colliders[0].GetComponent<IDamagable>(); 
+                        Attack(damagable);
+                }
+                
+        }
+
+        private void Attack(IDamagable damagable)
+        {
+                damagable.TakeDamage(damage);
+        }
 }
